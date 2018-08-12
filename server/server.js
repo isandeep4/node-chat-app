c02f68fde0363296c738dc9cbb661f71bdec1b67
isandeep4 <isandeepsahoo5@gmail.com>
2018-08-12 12:47:31 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
-var app = express();
+const http = require('http');
+const socketIO = require('socket.io');
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection' , (socket)=> {
+  console.log('new connection is connected');
+  socket.on('disconnect' , () => {
+    console.log('dieconnected from client');
+  })
+});
+
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
