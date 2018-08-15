51f17994c3c9d59372f8561da916a7190fb36e7b
isandeep4 <isandeepsahoo5@gmail.com>
2018-08-15 12:23:53 +0530

    greet user

--- server/server.js
+++ server/server.js
@@ -10,7 +10,19 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 io.on('connection' , (socket)=> {
-  console.log('new user is connected');
+  console.log('new user connected');
+
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:'Welcome to the chat App',
+    createdAt:new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'New user joined',
+    createdAt:new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
     console.log('createMessage',message);
     io.emit('newMessage',{
