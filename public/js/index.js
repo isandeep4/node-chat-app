269728a3fd2d92acc702939194e6f4b80c88885d
isandeep4 <isandeepsahoo5@gmail.com>
2018-08-16 18:57:57 +0530

    messages shows on browser

--- public/js/index.js
+++ public/js/index.js
@@ -7,4 +7,23 @@ socket.on('disconnect',() => {
 });
 socket.on('newMessage',(message) => {
   console.log('new message' , message );
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li)
+
+});
+socket.emit('createMessage',{
+  from:'hank',
+  text:"Hi"
+},(data)=>{
+  console.log('Got it' , data)
+})
+jQuery('#message-form').on('submit',function(e){
+  e.preventDefault();
+  socket.emit('createMessage',{
+    from:'user',
+    text: jQuery('[name=message]').val()
+  },function(){
+
+  });
 })
